42f7657a45e8c3eb303e5c3ca920860e0c305304
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-03-13 19:30:07 -0300

    :wrench: chore: set development variables

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'utd_php_module_04',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
